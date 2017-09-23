f5b28272cb5addc4c3dbf9d04833a0be0752724a
wahyudibo <wahyudi.ibo.wibowo@gmail.com>
2017-09-23 17:09:24 +0700

    only display update status button if report status is not DONE

--- resources/views/datatable/dashboard.blade.php
+++ resources/views/datatable/dashboard.blade.php
@@ -1,3 +1,5 @@
 <button type="button" class="btn btn-primary btn-view" value="{{ $id }}"><i class="fa fa-eye"></i></button>
-<button type="button" class="btn btn-success btn-update-status" value="{{ $id }}"><i class="fa fa-check"></i></button>
-<button type="button" class="btn btn-danger btn-delete" value="{{ $id }}"><i class="fa fa-trash"></i></button>
+@if ($status !== 'DONE')
+    <button type="button" class="btn btn-success btn-update-status" value="{{ $id }}"><i class="fa fa-check"></i></button>
+    <button type="button" class="btn btn-danger btn-delete" value="{{ $id }}"><i class="fa fa-trash"></i></button>
+@endif
